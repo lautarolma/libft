42fe2b2e4ef7df35e59016a8e72bfa64765499ee
lautarolma <lautaro.avilesleiva@gmail.com>
2025-10-01 16:22:46 +0200

    Trying

---
+++ ft_memcmp.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	int		dif;
+
+	if (!s1 || !s2)
+		return (0);
+	i = 0;
+	while (i < n)
+	if (s1[i] != s2[i]) //Not shure what  the functions do.. Check Man before to go on.
+	{
+		dif
+		i++;
+	}
+	return (n);
+}
+
+int	main()
+{
+	const void*	str1 = "ASDFGHJ"
+	const void*	str2 = "ASDFHJKL"
+	size_t		n
+}
