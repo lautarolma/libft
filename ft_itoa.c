ff716eecaa9cfd1aea7bd3abc6a571d66bf412df
Lautaro Aviles <laviles@c2r14s2.42madrid.com>
2025-10-16 17:41:26 +0200

    itoa done! Missing norminette check

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: laviles <laviles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 22:15:46 by laviles           #+#    #+#             */
-/*   Updated: 2025/10/16 05:39:51 by laviles          ###   ########.fr       */
+/*   Updated: 2025/10/16 17:40:28 by laviles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,17 @@ int	ft_ncounter(int n)
 
 void	ft_screator(int n, char *strnb, int len)
 {
-	strnb[len] = '\0';
 	if (n < 0)
-		strnb[0] = '-';
-	while (n > 0)
 	{
+		n *= -1;
+		strnb[0] = '-';
+	}
+	strnb[len] = '\0';
+	while (n != 0)
+	{
+		len--;
 		strnb[len] = (n % 10) + '0';
 		n = n / 10;
-		len--;
 	}
 }
 
@@ -47,50 +50,47 @@ char	*ft_itoa(int n)
 {
 	char	*strnb;
 	int		len;
-	long	nb;
 
-	nb = (long)n;
-	if (nb == INT_MIN)
-		return (ft_strdup("-2147483647"));
-	if (nb == 0)
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	if (n == 0)
 		return (ft_strdup("0"));
-	len = ft_ncounter(nb);
-	if (nb < 0)
-	{
+	len = ft_ncounter(n);
+	if (n < 0)
 		len++;
-		nb *= -1;
-	}
 	strnb = malloc((len + 1) * sizeof(char));
 	if (!strnb)
 		return NULL;
-	ft_screator(nb, strnb, len);
+	ft_screator(n, strnb, len);
 	return (strnb);
 }
-/*
+
 int	main()
 {
 	int test[] = {0, 5, -5, 10, -10, 12345, -12345, INT_MAX, INT_MIN};
-	char * str;
-
-	for (int i = 0; i < sizeof(test)/sizeof(int); i++)
+	char * str; 
+	int i = 0;
+    
+	while (i < 10) 
 	{
 		str = ft_itoa(test[i]);
 		printf("%d -> %s\n", test[i], str);
 		free(str);
+		i++;
 	}
 	return (0);
 }
-*/
+/*
 int	main()
 {
 	int		n;
 	char	*str;
 
-	n = 83648;
+	n = 0;
 	str = ft_itoa(n);
 	if (!str)
 		return (1);
 	printf("%s", str); 
 	free(str);
 	return (0);
-}
+}*/
